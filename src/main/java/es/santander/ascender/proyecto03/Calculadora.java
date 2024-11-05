8c6f07bd68bb092947e145a892e4171bc8c20836
curso15 <curso15@gmail.com>
2024-11-05 13:59:20 +0100

    calculadora

---
+++ src/main/java/es/santander/ascender/proyecto03/Calculadora.java
@@ -0,0 +1,20 @@
+package es.santander.ascender.proyecto03;
+
+public class Calculadora {
+    public long  sumar(long sumando1, long sumando2) {
+        return sumando1 + sumando2;
+    }
+
+    public long restar(long sustraendo, long minuendo){
+        return sustraendo - minuendo;
+    }
+
+    public long multiplicar(long factor, long factor2){
+         return factor * factor2;
+    }
+
+    public long dividir( long dividendo, long divisor){
+        return dividendo/divisor;
+    }
+
+}
